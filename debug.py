ad44fcb3e21f5b94180f28398a4c733067d55c1b
den-va <mironov.d@valiotti.com>
2024-11-05 14:47:13 +0100

    Update debug.py

--- debug.py
+++ debug.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     config = Config()
     token = config.get("slack", "bot_token")
     channel = config.get("slack", "debug_user")
-    # channel = config.get("slack", "debug_channel")
     slack = SlackClient(token)
-    result = slack.send_initial_block(channel, "2024-01-01")
-    # result = slack.clear_chat("D07TT531YF9")
-    # send to user id, chat.update channel id D07TT531YF9
+    result = slack.send_message(channel, "2024-01-01")
     print(result)
