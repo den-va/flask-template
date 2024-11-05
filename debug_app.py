fbb377f000098b1d4f4542ded0f1089a47e5fad2
den-va <mironov.d@valiotti.com>
2024-11-05 15:01:47 +0100

    Update debug_app.py

--- debug_app.py
+++ debug_app.py
@@ -5,18 +5,15 @@ from app.slack_api.slack_client import SlackClient
 
 
 app = Flask(__name__)
-user_selections = {}
 config = Config()
 token = config.get("slack", "bot_token")
-channel = config.get("slack", "debug_user")
 slack = SlackClient(token)
 
 
 @app.route("/slack/interact", methods=["POST"])
-def slack_events(selected_project=None, selected_task=None):
+def slack_events():
     payload = json.loads(request.form.get("payload"))
-    actions = payload["actions"]
-    action_id = actions[0]["action_id"]
+    action_id = payload["actions"][0]["action_id"]
     trigger_id = payload["trigger_id"]
 
     return "", 200
